e62f0f93ad9bdbecf0b1c0ff80fc44e345128a0e
praveen <rpraveen711@gmail.com>
2025-12-27 14:42:50 +1100

    Clean index.js with /, /status routes

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,13 @@ const PORT = 3000;
 app.get('/', (req, res) => {
     res.send('AegisAI backend is running');
 });
+app.get('/status', (req, res) => {
+    res.json({
+        service:'AegisAI',
+        version: '0.0.1',
+        status: 'running'
+    });
+});
 app.listen(PORT, () => {
     console.log(`AegisAI server running on port ${PORT}`);
 });
